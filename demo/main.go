ae9b88bb148cb651a3d35c9043af72e7431a1a9d
Eric Breuers <eric@ebdigital.ca>
2020-11-27 16:35:27 -0800

    update script for gwtf, remove wallet.json from old version

--- demo/main.go
+++ demo/main.go
@@ -6,28 +6,38 @@ import (
 )
 
 func main() {
-	gwtf := gwtf.NewGoWithTheFlowEmulator().
+	g := gwtf.
+		NewGoWithTheFlowEmulator().
 		InitializeContracts().
 		CreateAccount("user_1", "user_2")
 
-	// fmt.Println("Deploy contracts - press ENTER")
+	var ignoreFields = map[string][]string{
+		"flow.AccountCodeUpdated": []string{"codeHash"},
+		"flow.AccountKeyAdded": []string{"publicKey"},
+	}
 
-	// fmt.Scanln()
+	// Create a league with user 1
+	g.
+		TransactionFromFile("league/create_league").
+		SignProposeAndPayAs("user_1").
+		RunPrintEvents(ignoreFields)
 
-	// gwtf.DeployContract(Athletaverse)
+	// Create a team with user 1
+	g.
+		TransactionFromFile("team/create_team").
+		SignProposeAndPayAs("user_1").
+		RunPrintEvents(ignoreFields)
+	
+	// Register the team to the league
+	g.
+		TransactionFromFile("team/register_team").
+		SignProposeAndPayAs("user_1").
+		RunPrintEvents(ignoreFields)
 
-	// fmt.Println()
-	// fmt.Println()
-	// fmt.Println("Contracts successfully deployed!")
-
-	// gwtf.SendTransaction("league/create_league", Athletaverse)
-	// gwtf.SendTransaction("team/create_team", Athletaverse)
-	// gwtf.SendTransaction("team/register_team", Athletaverse)
-
-	// fmt.Println()
-	// fmt.Println()
-	// fmt.Println("Setup completed")
-
-	// gwtf.RunScript("league/get_team_ids")
+	// Check the team IDs registered to the league
+	g.
+		ScriptFromFile("league/get_team_ids").
+		AccountArgument("user_1").
+		Run()
 
 }
